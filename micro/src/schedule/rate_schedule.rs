c5b1e86dbb74b58cbd8e9008d4576a14f2e5c2de
Alberto Ielpo <alberto.ielpo@ennova-research.com>
2023-12-06 18:25:42 +0100

    1.0.4

--- micro/src/schedule/rate_schedule.rs
+++ micro/src/schedule/rate_schedule.rs
@@ -1,5 +1,5 @@
 use common_lib::provider::{redis::RedisProvider, redis_keys::CURRENT_RATE_VALUE};
-use log::{debug, info};
+use log::info;
 use std::time::Duration;
 use tokio::time::sleep;
 
@@ -11,7 +11,6 @@ pub async fn schedule_rate() {
         // generate rate
         let rate_dto = generate_rate();
         // update data to redis...
-        debug!("calling redis....");
         let _ = RedisProvider::set(CURRENT_RATE_VALUE.to_owned(), rate_dto.rate, None).await;
         sleep(Duration::from_secs(10)).await;
     }
